d2ff880487b2d0a8b1883f3d8eeb17b132facd78
Kaike Castro <42541528+kaikecc@users.noreply.github.com>
2021-03-19 18:02:05 -0300

    Update code.asm

--- LEDS-PROTEUS/code.asm
+++ LEDS-PROTEUS/code.asm
@@ -1,47 +1,118 @@
+.INCLUDE "m328Pdef.inc"
 
-	.INCLUDE "m328Pdef.inc"
+.ORG   0x0000                  // Tells the next instruction to be written
+rjmp   main                    // State that the program begins at the main label
 
-	.ORG   0x0000                  // Tells the next instruction to be written
-	RJMP   MAIN                    // State that the program begins at the main label
+main:
+	ldi r16, 0xFF             
+	out DDRB, r16 
+	out DDRD, r16             
+	
 
-	MAIN:
-	LDI R16, 0xFF               // Load the immedate value 0xFF (all bits 1) into register 16
-	OUT DDRB, R16               // Set Data Direction Register B to output for all pins
-	OUT DDRD, R16
+	
+	ldi r19, 0b00000000
+	ldi r20, 0b00000000
+	
+
+loop:
 
 	
 	
-	; 124
-
-	LOOP:
 	
-	sbi PORTB, PB0
-	cbi PORTB, PB1
-	cbi PORTB, PB2
-	cbi PORTB, PB3
-	call DELAY
+
+rjmp loop
+
+delay:
+	dec r18 ; decremente r18
+	brne delay ; salte para delay_loop se r18 n~ao ´e 0
+	dec r17 ; decremente r17
+	brne delay ; salte para delay_loop se r17 n~ao ´e 0
+	dec r19 ; decrament r19
+	brne delay ; salte para delay_loop se r19 n~ao ´e 0
+	ret ; retorne
 
 
-	sbi PORTB, PB0
-	sbi PORTB, PB1
-	cbi PORTB, PB2
-	cbi PORTB, PB3
-	call DELAY
+led_run:
 
-	sbi PORTB, PB0
-	cbi PORTB, PB1
-	sbi PORTB, PB2
-	cbi PORTB, PB3
-	call DELAY
+	out PORTB, r19
 
 
-	RJMP LOOP
+led_timerH:
 
-	DELAY:
-    dec r18 ; decremente r18
-    brne DELAY ; salte para delay_loop se r18 n~ao ´e 0
-    dec r17 ; decremente r17
-    brne DELAY ; salte para delay_loop se r17 n~ao ´e 0
-    dec r19 ; decrament r19
-    brne DELAY ; salte para delay_loop se r19 n~ao ´e 0
-    ret ; retorne
+	sbi PORTD, PD4 ; LIGA O LED T
+
+	led_timerM:
+		
+	sbi PORTD, PD4 ; LIGA O LED T
+	call delay
+	cbi PORTD, PD4 ; DESLIGA O LED T
+	call delay
+		
+	rjmp led_timerM
+
+led_Week:
+
+	sbi PORTD, PD4 ; LIGA O LED T
+	sbi PORTB, PB0 ; LIGA O LED D1
+
+	sbi PORTD, PD5 ; LIGA O LED W
+	call delay
+	cbi PORTD, PD5 ; DESLIGA O LED W
+	call delay
+
+	rjmp led_Week
+
+
+led_OnH:
+
+	sbi PORTD, PD6 ; LIGA O LED O
+
+
+led_OnM:
+
+	sbi PORTD, PD6 ; LIGA O LED O
+	call delay
+	cbi PORTD, PD6 ; DESLIGA O LED O
+	call delay
+
+		
+		
+
+led_WeekOn:
+
+		
+	sbi PORTD, PD6 ; LIGA O LED O
+
+	sbi PORTB, PB0 ; LIGA O LED D1
+
+	sbi PORTD, PD5 ; LIGA O LED W
+	call delay
+	cbi PORTD, PD5 ; DESLIGA O LED W
+	call delay
+
+		
+
+led_OffM:
+
+	sbi PORTD, PD7 ; LIGA O LED F
+	call delay
+	cbi PORTD, PD7 ; DESLIGA O LED F
+	call delay
+
+	rjmp led_OffM
+
+led_OffH:
+
+		sbi PORTD, PD7 ; LIGA O LED F
+
+led_WeekOff:
+
+		sbi PORTD, PD7 ; LIGA O LED F
+		sbi PORTB, PB0 ; LIGA O LED D1
+
+		sbi PORTD, PD5 ; LIGA O LED W
+		call delay
+		cbi PORTD, PD5 ; DESLIGA O LED W
+		call delay
+
+		rjmp led_WeekOff
